bbb7f8478d313d10756b10b50ae6682916f0f663
Leonardo Castorina <leonardo.castorina98@gmail.com>
2019-01-08 19:02:42 +0100

    Refactor using config.py

--- bac_detection/predict.py
+++ bac_detection/predict.py
@@ -1,40 +1,34 @@
 from keras.models import model_from_json
 
 
-def load_model(path):
+def _load_model(model_path, loss_func, optimizer, metrics):
     """
-    :param path: string
+    :param model_path: string
     :return: model: keras model object
     """
 
-    print("Loading model")
-    # TODO: Refactor magic variable
-    with open(path+'model.json', 'r') as json_file:
+    print("Loading model...")
+    with open(model_path + 'model.json', 'r') as json_file:
         model_json = json_file.read()
 
     # Load model in Keras:
     model = model_from_json(model_json)
 
     # Load weights:
-    # TODO: Refactor magic variable
-    model.load_weights(path+'model.h5')
-    # TODO: Refactor magic variable
-    model.compile(loss='mean_squared_error', optimizer='adamax', metrics=[
-        'accuracy'])
+    model.load_weights(model_path + 'model.h5')
+    model.compile(loss=loss_func, optimizer=optimizer, metrics=metrics)
 
     return model
 
 
-def predict_flatness(X_test):
+def predict_flatness(X_test, model_path, loss_func, optimizer, metrics):
     """ Uses the model to predict flatness. Must be given path argument.
     :param X_test: 4d numpy array with bacterial images
+    :param model_path: str path to models
     :return prediction: float probability of flatness
     """
 
-    # TODO: Refactor magic variable
-    flat_path = 'neural_network/models/'
-
-    model = load_model(flat_path)
+    model = _load_model(model_path, loss_func, optimizer, metrics)
     prediction = model.predict(X_test)
 
     return prediction
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+from config import *
 
 from skimage.io import imread
 
@@ -6,41 +6,31 @@ from bac_detection.image_processing import thresholding_img
 from bac_detection.image_processing import rectify_bac
 from bac_detection.image_processing import preprocess_cutout
 from bac_detection.output import outwrite_coords, outwrite_graph
-
 from bac_detection.predict import predict_flatness
 
+
 if __name__ == '__main__':
-    # TODO: create config.py
-    PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
-    data_path = os.path.join(PROJECT_ROOT_DIR,
-                             'bacxeption/neural_network/test/')
-    output_graph = True
-    output_coordinates = True
+    print(f'Loading data from {TEST_DIR}')
 
-    print(f'Loading data from {data_path}')
-
-    id_dict = {}
-
-    # Loop through all images:
-    for filename in os.listdir(data_path):
-        # TODO: variable in config
-        if filename.endswith('.tiff') or filename.endswith('.tif'):
+    # Loop through all test images:
+    for filename in os.listdir(TEST_DIR):
+        if filename.endswith(FORMAT_IMG):
             print(f'Processing {filename}')
-            # TODO: variable in config
-            bac_img = imread(os.path.join(data_path, filename), as_gray=True)
+            bac_img = imread(os.path.join(TEST_DIR, filename), as_gray=True)
             thresh_img = thresholding_img(bac_img)
 
-            # Extract rectangles in image:
+            # Extract rectangles of bacteria in image:
             coords_list, cutouts_list = rectify_bac(thresh_img, bac_img)
 
             # Normalize cutouts for network dimensions:
             norm_cutouts = preprocess_cutout(cutouts_list)
-            flat_results = predict_flatness(norm_cutouts)
+            flat_results = predict_flatness(norm_cutouts, LOG_DIR,
+                                            LOSS_FUNC,  OPTIMIZER, METRICS)
 
             filename = filename.split('.')[0]
-            # Output Coordinates if True:
-            if output_coordinates:
-                outwrite_coords(coords_list, flat_results, data_path, filename)
-            if output_graph:
+
+            if OUTPUT_COORDS:
+                outwrite_coords(coords_list, flat_results, TEST_DIR, filename)
+            if OUTPUT_GRAPH:
                 outwrite_graph(bac_img, coords_list, flat_results,
-                               data_path, filename)
+                               TEST_DIR, filename)
