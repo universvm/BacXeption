0a29c6a26dda5ad7d714605f224945eae8d1b332
universvm <leonardo.castorina98@gmail.com>
2019-01-10 22:19:24 +0100

    #5 Integrate uninitialized Xception module

--- neural_network/network_structure.py
+++ neural_network/network_structure.py
@@ -1,3 +1,4 @@
+from keras.applications import Xception
 from keras.layers import Dense, Flatten, Input
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
@@ -17,33 +18,45 @@ def convolve(input_layer, conv_depth, kernel_size, pool_size):
     return MaxPooling2D(pool_size=(pool_size, pool_size))(conv_3)
 
 
-def build_network():
+# def build_network():
+#     """ Main CNN to predict flat or not_flat  """
+#
+#     # Hyperparameters:
+#     conv_depth = [32, 64, 128]
+#     kernel_size = 3
+#     pool_size = 3
+#     hidden_size = 50
+#
+#     # Network structure:
+#     inp_layer = Input(shape=(180, 180, 1))
+#
+#     conv_layer = inp_layer
+#     for i in range(3):
+#         conv_layer = convolve(conv_layer, conv_depth[i], kernel_size,
+#                               pool_size)
+#     flat = Flatten()(conv_layer)
+#
+#     hidden = Dense(hidden_size, activation='relu')(flat)
+#     softmax = Dense(2, activation='softmax')(hidden)
+#     model = Model(inputs=inp_layer, outputs=softmax)
+#
+#     model.compile(loss='mean_squared_error', optimizer='adamax',
+#                   metrics=['accuracy'])
+#
+#     # Output summary:
+#     print(model.summary())
+#
+#     return model
+
+
+def build_network(inp_shape, num_classes, optimizer, loss_func, metrics):
     """ Main CNN to predict flat or not_flat  """
 
-    # Hyperparameters:
-    conv_depth = [32, 64, 128]
-    kernel_size = 3
-    pool_size = 3
-    hidden_size = 50
+    model = Xception(weights=None, input_shape=inp_shape, classes=num_classes)
 
-    # Network structure:
-    inp_layer = Input(shape=(180, 180, 1))
-
-    conv_layer = inp_layer
-    for i in range(3):
-        conv_layer = convolve(conv_layer, conv_depth[i], kernel_size,
-                              pool_size)
-    flat = Flatten()(conv_layer)
-
-    hidden = Dense(hidden_size, activation='relu')(flat)
-    softmax = Dense(2, activation='softmax')(hidden)
-    model = Model(inputs=inp_layer, outputs=softmax)
-
-    model.compile(loss='mean_squared_error', optimizer='adamax',
-                  metrics=['accuracy'])
+    model.compile(loss=loss_func, optimizer=optimizer, metrics=metrics)
 
     # Output summary:
     print(model.summary())
 
     return model
-
